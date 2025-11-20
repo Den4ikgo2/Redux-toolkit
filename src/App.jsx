ac2131bbf5a4003ff550973ab978c48cfa3ebb23
Den4ikgo2 <149091376+Den4ikgo2@users.noreply.github.com>
2025-11-20 13:48:31 +0600

    Fix/App - added a user to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
 import RecipeItem from "./components/recipe-item/RecipeItem";
+import User from "./components/user/user";
 
 function App() {
   return (
     <section>
       <Header />
+      <User />
       <div>
         <RecipeItem
           recipe={{
