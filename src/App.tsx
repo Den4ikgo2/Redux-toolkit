8bedbbb938ba3a8996eee3f5814f71e16e9cd6fa
Den4ikgo2 <149091376+Den4ikgo2@users.noreply.github.com>
2025-11-20 19:51:46 +0600

    Feat/filter - add filter in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import CreateRecipe from "./components/create-recipe/CreateRecipe";
 import Header from "./components/header/Header";
 import RecipeItem from "./components/recipe-item/RecipeItem";
@@ -5,12 +6,29 @@ import User from "./components/user/User";
 import { useGetRecipesQuery } from "./store/api/api";
 
 function App() {
-  const { isLoading, data } = useGetRecipesQuery(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [queryTerm, setQueryTerm] = useState('')
+
+  const { isLoading, data } = useGetRecipesQuery(queryTerm);
+
+  const handleSearch = () => {
+    setQueryTerm(searchTerm)
+  };
 
   return (
     <section>
       <Header />
       <CreateRecipe />
+      <p>If you wanna find: </p>
+      <div>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter search term"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <User />
       {isLoading && <h2>Идет загрузка...</h2>}
       <ul>
--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -10,11 +10,12 @@ export const api = createApi({
     baseUrl: API_URL,
   }),
   endpoints: (builder) => ({
-    getRecipes: builder.query<IRecipe[], null>({
-      query: () => "/?_sort=id&_order=desc",
-      providesTags: () => [
+    getRecipes: builder.query<IRecipe[], string>({
+      query: (searchTerm) => `/?_sort=id&_order=desc&q=${searchTerm}`,
+      providesTags: (result, error, searchTerm) => [
         {
           type: "Recipe",
+          id: searchTerm
         },
       ],
     }),
