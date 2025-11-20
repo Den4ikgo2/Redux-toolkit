87645290097020d55cf66943346cfb3765107f31
Den4ikgo2 <149091376+Den4ikgo2@users.noreply.github.com>
2025-11-20 17:30:47 +0600

    Fix/app - update app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CreateRecipe from "./components/create-recipe/CreateRecipe";
 import Header from "./components/header/Header";
 import RecipeItem from "./components/recipe-item/RecipeItem";
 import User from "./components/user/user";
@@ -9,15 +10,14 @@ function App() {
   return (
     <section>
       <Header />
+      <CreateRecipe />
       <User />
       {isLoading && <h2>Идет загрузка...</h2>}
       <ul>
         {data ? (
           data.map((item) => (
             <li key={item.id}>
-              <RecipeItem
-                recipe={item}
-              />
+              <RecipeItem recipe={item} />
             </li>
           ))
         ) : (
